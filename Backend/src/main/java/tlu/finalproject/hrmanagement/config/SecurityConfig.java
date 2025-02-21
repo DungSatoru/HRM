6950af51f68b9b6d380aae22e80f683461ba6f65
dungsatoru <dungsatoru.dev@gmail.com>
2025-02-21 20:19:54 +0700

    Create Config(ModelMapper, Security)

--- Backend/src/main/java/tlu/finalproject/hrmanagement/config/SecurityConfig.java
+++ Backend/src/main/java/tlu/finalproject/hrmanagement/config/SecurityConfig.java
@@ -1,8 +1,22 @@
 package tlu.finalproject.hrmanagement.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())  // Tắt CSRF nếu chỉ dùng API
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users/**").permitAll()  // Cho phép truy cập API users
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
 }
