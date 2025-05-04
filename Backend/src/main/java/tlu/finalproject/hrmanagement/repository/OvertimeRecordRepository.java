824dc5a65b394af3b3e7bb5587168322ad450bc9
dungsatoru <dungsatoru.dev@gmail.com>
2025-05-04 14:05:09 +0700

    feat: Introduce OvertimeRecordDTO and OvertimeRecordProjection for enhanced overtime record management; update OvertimeRecordRepository to use projections in queries

--- Backend/src/main/java/tlu/finalproject/hrmanagement/repository/OvertimeRecordRepository.java
+++ Backend/src/main/java/tlu/finalproject/hrmanagement/repository/OvertimeRecordRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import tlu.finalproject.hrmanagement.dto.OvertimeRecordDTO;
 import tlu.finalproject.hrmanagement.model.OvertimeRecord;
+import tlu.finalproject.hrmanagement.projection.OvertimeRecordProjection;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,25 +15,24 @@ import java.util.Optional;
 @Repository
 public interface OvertimeRecordRepository extends JpaRepository<OvertimeRecord, Long> {
 
-//    List<OvertimeRecord> findByUser_UserIdAndMonth(Long userId, String monthYearString);
+//   List<OvertimeRecord> findByUser_UserIdAndMonth(Long userId, String monthYearString);
 
     @Query(value = """
-    SELECT 
-        YEAR(overtime_date) AS year,
-        MONTH(overtime_date) AS month,
-        SUM(overtime_hour) AS total_overtime
-    FROM 
-        overtime_records
-    WHERE 
-        user_id = :userId
-        AND DATE_FORMAT(overtime_date, '%Y-%m') = :monthYearString
-    GROUP BY 
-        YEAR(overtime_date), MONTH(overtime_date)
-    ORDER BY 
-        year, month
-    """, nativeQuery = true)
-    List<OvertimeRecord> findByUser_UserIdAndMonth(@Param("userId") Long userId, @Param("monthYearString") String monthYearString);
-
+                SELECT 
+                    o.overtime_id AS overtimeId, 
+                    o.user_id AS userId, 
+                    o.overtime_start AS overtimeStart, 
+                    o.overtime_end AS overtimeEnd, 
+                    o.overtime_hour AS overtimeHour, 
+                    o.overtime_pay AS overtimePay, 
+                    o.overtime_date AS overtimeDate, 
+                    CONCAT(EXTRACT(YEAR FROM o.overtime_date), '-', LPAD(EXTRACT(MONTH FROM o.overtime_date), 2, '0')) AS month
+                FROM overtime_records o
+                WHERE o.user_id = :userId
+                AND CONCAT(EXTRACT(YEAR FROM o.overtime_date), '-', LPAD(EXTRACT(MONTH FROM o.overtime_date), 2, '0')) = :monthYear
+                ORDER BY o.overtime_date
+            """, nativeQuery = true)
+    List<OvertimeRecordProjection> findByUserIdAndMonth(@Param("userId") Long userId, @Param("monthYear") String monthYear);
 
     void deleteByUser_UserIdAndOvertimeDate(Long userId, LocalDate date);
 
